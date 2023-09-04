f8202e2534255b1a2582176ee2f0b8718199c33a
sotetu-79 <sotetu79@gmail.com>
2023-09-04 14:48:03 +0900

    開発物紹介ページのレスポンシブ対応

--- src/components/templates/WorkPage.jsx
+++ src/components/templates/WorkPage.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Box, Center, Text, Button, HStack, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Text,
+  Button,
+  HStack,
+  VStack,
+  useMediaQuery,
+  Flex,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import Thumbnail from "../molecules/Thumbnail";
 
 function WorkPage({ src, name, url, github, zenn, description }) {
+  const [isLessThan1280] = useMediaQuery("(max-width: 1280px)");
+
   return (
     <Box w="100vw" h="100vh">
       <Center h="100vh">
         <Box
           w="75rem"
-          h="40rem"
+          h={isLessThan1280 ? "55rem" : "40rem"}
           bg="white"
           mt="3rem"
           borderRadius="md"
@@ -20,8 +31,8 @@ function WorkPage({ src, name, url, github, zenn, description }) {
             <Text
               as="a"
               position="absolute"
-              top="1rem"
-              left="1rem"
+              top={isLessThan1280 ? "8rem" : "1rem"}
+              left={isLessThan1280 ? "2rem" : "1rem"}
               cursor="pointer"
               fontWeight="bold"
               fontSize="lg"
@@ -31,9 +42,18 @@ function WorkPage({ src, name, url, github, zenn, description }) {
               ←ホームに戻る
             </Text>
           </Link>
-          <HStack position="absolute" top="8rem" left="3rem" align="start">
+          <Flex
+            position="absolute"
+            top="8rem"
+            left="0"
+            right="0"
+            pl="8"
+            mt="5"
+            align={isLessThan1280 ? "center" : "start"}
+            direction={isLessThan1280 ? "column" : "row"}
+          >
             <Thumbnail src={src} />
-            <VStack pl="6" align="start">
+            <VStack pl="6" align="center">
               <Text variant="primary" fontWeight="bold" fontSize="4.2rem">
                 {name}
               </Text>
@@ -68,11 +88,11 @@ function WorkPage({ src, name, url, github, zenn, description }) {
                   </Button>
                 )}
               </HStack>
-              <Text variant="primary" fontSize="xl" pt="6">
+              <Text variant="primary" fontSize="xl" pt="6" pr="2">
                 {description}
               </Text>
             </VStack>
-          </HStack>
+          </Flex>
         </Box>
       </Center>
     </Box>
